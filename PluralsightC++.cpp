83a797b385d10c040d014d7f4a021963d25ae6fa
Jeremy Ng <jngchenghin@outlook.com>
2023-02-16 00:00:18 -0800

    Display name

--- PluralsightC++.cpp
+++ PluralsightC++.cpp
@@ -1,7 +1,12 @@
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "What is your name?" << std::endl;
+    std::string name;
+    std::getline(std::cin, name);
+    std::cout << name << " is a lovely name, Welcome!" << std::endl;
+
+    return 0;
 }
